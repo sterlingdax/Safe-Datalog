eaa1ac6657d9bcbb1eacdd60fa05a3c9323a1bf6
Sterling <sterlingdax@Air.local>
2016-08-30 19:21:55 -0400

    Safe

---
+++ datalogger.py
@@ -0,0 +1,14 @@
+# Read datalogger from safe
+
+import csv
+
+# So yeah, get filename, open that file, then print the contents
+# Not the most glamorous, but hey, it's a start!
+filename = raw_input("Filename: ")
+data = open(filename)
+dataReader = csv.reader(data)
+for row in data:
+	print
+
+
+
