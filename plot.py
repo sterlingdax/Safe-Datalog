f4221519f56c6879aee07b0d1112dd540aedf841
Sterling <sterlingdax@users.noreply.github.com>
2016-09-08 18:31:06 -0400

    work

--- plot.py
+++ plot.py
@@ -1,12 +1,25 @@
 # From this page
 # https://www.raspberrypi.org/learning/astro-pi-flight-data-analysis/graphing/
-# Currently just gives temp, needs to be worked on
 # Ask for filename, add dates, humidity, etc
 
 from csv import reader
 from matplotlib import pyplot
-with open('DATALOG3-11to7-20-2016.txt', 'r') as f:
+from dateutil import parser
+
+with open('test.csv', 'r') as f:
     data = list(reader(f))
 temp = [i[1] for i in data]
+humidity = [i[0] for i in data]
+battery = [i[2] for i in data]
+#date = [i[3] for i in data]
+#time = [parser.parse(i[4]) for i in data[1::]]
+pyplot.title('Safe Temp vs Time')
+pyplot.xlabel('Time/hours')
+pyplot.ylabel('Temp/brrr!')
+#pyplot.plot(time,temp)
 pyplot.plot(range(len(temp)), temp)
+pyplot.plot(range(len(humidity)), humidity)
+pyplot.plot(range(len(battery)), battery)
+#pyplot.plot(range(len(date)), date)
+#pyplot.plot(range(len(time)), time)
 pyplot.show()
