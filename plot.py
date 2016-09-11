74f5d49b8b5ff0e683473c7339484358d7529ff8
Sterling <sterlingdax@users.noreply.github.com>
2016-09-11 12:11:07 -0400

    work

--- plot.py
+++ plot.py
@@ -3,23 +3,15 @@
 # Ask for filename, add dates, humidity, etc
 
 from csv import reader
-from matplotlib import pyplot
+from matplotlib import pyplot, dates
 from dateutil import parser
 
 with open('test.csv', 'r') as f:
     data = list(reader(f))
 temp = [i[1] for i in data]
-humidity = [i[0] for i in data]
-battery = [i[2] for i in data]
-#date = [i[3] for i in data]
-#time = [parser.parse(i[4]) for i in data[1::]]
+time = [parser.parse(i[4]) for i in data]
 pyplot.title('Safe Temp vs Time')
 pyplot.xlabel('Time/hours')
 pyplot.ylabel('Temp/brrr!')
-#pyplot.plot(time,temp)
-pyplot.plot(range(len(temp)), temp)
-pyplot.plot(range(len(humidity)), humidity)
-pyplot.plot(range(len(battery)), battery)
-#pyplot.plot(range(len(date)), date)
-#pyplot.plot(range(len(time)), time)
+pyplot.plot(time, temp)
 pyplot.show()
